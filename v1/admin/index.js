44462339d00deb7babaefe087a342e84fbcdaa3e
Zach Caldwell <24858526+caldwellz@users.noreply.github.com>
2024-03-14 19:13:55 -0600

    Implement /admin/users endpoints

--- v1/admin/index.js
+++ v1/admin/index.js
@@ -3,12 +3,11 @@ const router = Router();
 
 // Only admins can access this section of the API
 router.use((req, res, next) => {
-  const { role } = req.user;
-  if (role === 'admin' || role === 'superadmin') return next();
-  res.sendStatus(404);
+  if (req.user?.hasRole?.('admin')) return next();
+  res.status(403).json({ success: false, error: 'Unauthorized' });
 });
 
 router.use('/domains', require('./domains.js'));
-router.use('/keys', require('./keys.js'));
+router.use('/users', require('./users.js'));
 
 module.exports = router;
---
+++ v1/admin/users.js
@@ -0,0 +1,115 @@
+const { celebrate, Joi } = require('celebrate');
+const { Router } = require('express');
+const User = require('../../models/User.js');
+const router = Router();
+
+function transformUser(user) {
+  // Rename the id and omit passwordHash and apiKeys
+  const { _id: id, username, roles, active } = user;
+  return { id, username, roles, active };
+}
+
+router.get(
+  '/:userId',
+  celebrate({
+    params: Joi.object({
+      userId: Joi.string().hex().length(24),
+    }),
+  }),
+  async (req, res, next) => {
+    const { userId } = req.params;
+    const existingUser = await User.findOne({ _id: userId }).exec();
+    if (!existingUser) return next(); // Fall back to 404 handler
+    res.json({ success: true, user: transformUser(existingUser) });
+  }
+);
+
+router.get(
+  '/',
+  celebrate(
+    {
+      query: Joi.object({
+        username: Joi.string().alphanum(),
+        userId: Joi.string().hex().length(24),
+      })
+        .or('username', 'userId')
+        .required(),
+    },
+    { stripUnknown: true }
+  ),
+  async (req, res, next) => {
+    const { userId: _id, ...queryParams } = req.query;
+    if (_id) queryParams._id = _id;
+    const existingUser = await User.findOne(queryParams).exec();
+    if (!existingUser) return next(); // Fall back to 404 handler
+    res.json({ success: true, user: transformUser(existingUser) });
+  }
+);
+
+router.put(
+  '/',
+  celebrate({
+    body: Joi.object({
+      username: Joi.string().alphanum().lowercase().min(4).max(24).required(),
+      password: Joi.string().min(8).max(1024).required(),
+      roles: Joi.array().items(Joi.string().alphanum().required()).default(['user']),
+    }),
+  }),
+  async (req, res) => {
+    const { username, password, roles } = req.body;
+    const existingUser = await User.findOne({ username }).exec();
+    if (existingUser)
+      return res.status(400).json({ statusCode: 400, error: 'Bad Request', message: 'Username already taken' });
+
+    const user = new User({ username, roles, active: true });
+    const apiKey = await user.addAPIKey();
+    await user.updatePassword(password);
+    await user.save();
+    res.json({ success: true, apiKey, user: transformUser(user) });
+  }
+);
+
+router.patch(
+  '/:userId',
+  celebrate(
+    {
+      params: Joi.object({
+        userId: Joi.string().hex().length(24).required(),
+      }),
+      body: Joi.object({
+        active: Joi.boolean(),
+        password: Joi.string().min(8).max(1024),
+        roles: Joi.array().items(Joi.string().alphanum()),
+        username: Joi.string().alphanum().lowercase().min(4).max(24),
+      }),
+    },
+    { stripUnknown: true }
+  ),
+  async (req, res, next) => {
+    const { userId } = req.params;
+    const { password, ...changes } = req.body;
+    const user = await User.findById(userId).exec();
+    if (!user) return next(); // Fall back to 404 handler
+    Object.assign(user, changes);
+    if (password) await user.updatePassword(password);
+    await user.save();
+    res.json({ success: true, user: transformUser(user) });
+  }
+);
+
+router.delete(
+  '/:userId',
+  celebrate({
+    params: Joi.object({
+      userId: Joi.string().hex().length(24).required(),
+    }),
+  }),
+  async (req, res, next) => {
+    const { userId } = req.params;
+    const oldUser = await User.findByIdAndDelete(userId).exec();
+    if (!oldUser) return next(); // Fall back to 404 handler
+    res.json({ success: true });
+  }
+);
+
+module.exports = router;
